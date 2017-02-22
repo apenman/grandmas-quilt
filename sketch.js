e79cb7805a07d289d81a6ff469b9716ff3ded38a
Alex <apenman427@gmail.com>
2017-02-21 17:28:04 -0800

    Translation layer in place

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 /* TODO:
- * 1. MAYBE USE WEBGL SO I CAN THROW ON TRANSLATES TO SET PATTERN FUNCTIONS IN RIGHT PLACE
- *    CAN YOU DO TRANSLATE IN REGULAR MODE??
+ * 1. Currently hardcoded to 10x10 grid...change that
+ * 2. Pull patterns from external js files
 */
 
 var cells = [];
+var alternate = false;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   // set up quilt grid
+  setupGrid();
   // load patterns from external files?
 }
 
@@ -21,24 +23,38 @@ function draw() {
   displayPatterns();
 }
 
+function setupGrid() {
+  for(var i = 0; i < 10; i++) {
+    cells[i] = [];
+    for(var j = 0; j < 10; j++) {
+       cells[i][j] = new Cell(createVector(0, 0), createVector((windowWidth/10), (windowHeight/10)));
+    }
+  }
+}
+
 function makeGrid() {
   for(var i = 0; i < 10; i++) {
     line(0, i*(windowHeight/10), windowWidth, i*(windowHeight/10));
     line(i*(windowWidth/10), 0, i*(windowWidth/10), windowHeight);
-    cells[i] = [];
-    for(var j = 0; j < 10; j++) {
-      cells[i][j] = new Cell(createVector(i*(windowWidth/10), j*(windowHeight/10)), createVector((i+1)*(windowWidth/10), (j+1)*(windowHeight/10)));
-    }
   }
 }
 
 function displayPatterns() {
   for(var i = 0; i < cells.length; i++) {
-    console.log("AT I " + i);
+    // push on x translation
+    // pop off after each loop because we multiply offset by index
+    push();
+    translate(i*(windowWidth/10),0);
     for(var j = 0; j < cells[i].length; j++) {
-      console.log("AT J " + j);
+      // push on y translation
+      // pop off after each loop because we multiply offset by index
+      push();
+      translate(0,j*(windowHeight/10));
+      // Call the display function of the current cell
       cells[i][j].display();
+      pop();
     }
+    pop();
   }
 }
 
@@ -47,13 +63,9 @@ function XPattern(boxMin, boxMax) {
   this.boxMax = boxMax;
 
   this.display = function() {
-    console.log("MIN / MAX", boxMin, boxMax);
     stroke(random(255), random(255), random(255));
-    for(var i = 1; i < 4; i++) {
-      console.log("DRAWING LINE " + i + " at " + i*((this.boxMax.y-this.boxMin.y)/3));
-      line(this.boxMin.x, i*((this.boxMax.y-this.boxMin.y)/3)+this.boxMin.y, this.boxMax.x, i*((this.boxMax.y-this.boxMin.y)/3)+this.boxMin.y);
-      line(i*((this.boxMax.x-this.boxMin.x)/3)+this.boxMin.x, this.boxMin.y, i*((this.boxMax.x-this.boxMin.x)/3)+this.boxMin.x, this.boxMax.y);
-    }
+    line(this.boxMin.x,this.boxMax.y, this.boxMax.x, this.boxMin.y);
+    line(this.boxMin.x, this.boxMin.y, this.boxMax.x, this.boxMax.y)
   }
 }
 
@@ -62,15 +74,20 @@ function DotPattern(boxMin, boxMax) {
   this.boxMax = boxMax;
 
   this.display = function() {
-    ellipse(boxMax.x/2,boxMax.y/2, 10);
+    ellipse(boxMax.x/3,boxMax.y/3, 10);
+    ellipse(2*(boxMax.x/3),boxMax.y/3, 10);
+    ellipse(2*(boxMax.x/3),2*(boxMax.y/3), 10);
+    ellipse(boxMax.x/3,2*(boxMax.y/3), 10);
   }
 }
 
 function Cell(cellMin, cellMax) {
-  // this.cellMin = cellMin;
-  // this.cellMax = cellMax;
-  // this.pattern = new XPattern(cellMin, cellMax);
-  this.pattern = new DotPattern(cellMin, cellMax);
+  if(alternate)
+    this.pattern = new XPattern(cellMin, cellMax);
+  else
+    this.pattern = new DotPattern(cellMin, cellMax);
+  alternate = !alternate;
+
   this.display = function() {
     this.pattern.display();
   }
